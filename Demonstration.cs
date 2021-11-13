adbf9a9955e5a4ae8a9417ccced35f53480d7313
Kós Márton <kosmarci73@gmail.com>
2021-11-13 18:42:22 +0100

    Updated the Framework and the demo file

--- Demonstration.cs
+++ Demonstration.cs
@@ -1,8 +1,9 @@
-﻿// Demonstration file, only to showcase and give an example to how you can implement the API
+﻿/** Demonstration file, only to showcase and give an example to how you can implement and
+ * use the Framework. */
 
 using System;
 using Renci.SshNet;
-using ServerGateway; // Include the API's namespace
+using ServerGateway; /** Include the API's namespace. */
 
 class Program
 {
@@ -10,21 +11,27 @@ class Program
     {
         Console.WriteLine("Starting operation...");
 
-        // Server informations that the API needs to connect to the server through SSH
-        string username = "yourserverusername";
-        string ipAddress = "yourserveripaddress";
-        string password = "yourserverpassword";
+        /** Server informations what the Framework will use to connect the SSH and SFTP clients. */
+        string username = "serverusername";
+        string ipAddress = "serveripadress";
+        string password = "serverpassword";
+
+        /** A simple Linux bash command for just showcase. */
         string command = "echo 'Welcome to Linux (:'";
-        
-        // Creates an instance of the Service
+
+        /** Creates an instance of the ServerGateway class that contains all the Framework's 
+         * definitions and functions. */
         ServerGatewayService serverGatewayService = new ServerGatewayService(username, ipAddress, password);
-        // Using a function on the instance to get some basic information about the server
+        /** Using a function on the instance to get some basic information about the server. */
         SshCommand infos = serverGatewayService.GetServerInformations();
         Console.WriteLine(infos.Result);
 
-        // Sends a task through the API
+        /** Sends a task through the Framework's SSH client. */
         SshCommand sshCommand = serverGatewayService.SendTask(command);
         Console.WriteLine(sshCommand.Result);
+
+        /** Download a file from the server into this machine. */
+        serverGatewayService.DownloadFile("/var/local/image-2.png", @"C:\dev\");
 
         Console.WriteLine("Operation exited successfully!");
     }
--- ServerGatewayService.cs
+++ ServerGatewayService.cs
@@ -1,62 +1,96 @@
-﻿// Copyright Márton Kós Hungary 2021. All Rights Reserved
+﻿/** Copyright Márton Kós Hungary 2021. All Rights Reserved
+ * CSharp SSH and SFTP Framework for Visual Studio Integration */
 
 using System;
 using System.IO;
 using Renci.SshNet;
 using System.Text;
 
-namespace ServerGateway
+namespace ServerGateway /** Include namespace if the Framework class and the implemetation definitions are not in the same namespace. */
 {
-    /** The class for the entire API
-     * This class contains all the functions what the API comes with */
-    class ServerGatewayService
+    /** This class contains all the definitions and functions that this Framework comes with.
+     * After the contruction of the class instance finishes all the functions all usable if
+     * the given class parameters are valid. Construction speed depends on internet speed.*/
+    public sealed class ServerGatewayService
     {
         /** Constructor thats sets up the connection with the server an populates the internal variables
-         * And if the connection was successfull then sends a message through Console
-         * ISSUE: if the give IP address is invalid, the program crashes */
+         * and if the connection was successfull then sends a message through Console.
+         * ISSUE: if the give IP address is invalid, the program crashes. */
         public ServerGatewayService(string username, string ipAddress, string password)
         {
             m_Username = username;
             m_IpAddress = ipAddress;
             m_Password = password;
 
-            AuthenticationMethod method = new PasswordAuthenticationMethod(m_Username, m_Password);
-            ConnectionInfo connection = new ConnectionInfo(m_IpAddress, m_Username, method);
-            client = new SshClient(connection);
-            if (!client.IsConnected)
+            /** Sets up the SSH connection client through which information is 
+             * sent by the Framework. */
+            try
             {
-                Console.WriteLine($"Connected server on: {m_IpAddress} in user: {m_Username}");
-                client.Connect();
+                AuthenticationMethod method = new PasswordAuthenticationMethod(m_Username, m_Password);
+                ConnectionInfo connection = new ConnectionInfo(m_IpAddress, m_Username, method);
+                m_SshClient = new SshClient(connection);
+                if (!m_SshClient.IsConnected)
+                {
+                    LogMessage($"Connected server on: {m_IpAddress} in user: {m_Username}");
+                    m_SshClient.Connect();
+                }
             }
-            SshCommand readCommand = client.RunCommand("uname -mrs");
-            Console.WriteLine("Server's version: " + readCommand.Result);
+            catch (Exception ex)
+            {
+                LogError("Failed to set up the SSH connenction. Please check your server login information.");
+                LogError($"Detailed exception: {ex}");
+                Environment.Exit(1);
+            }
+            
+            /** Sets up the SFTP connection client through which files is 
+             * sent and received by the Framework. */
+            try
+            {
+                m_SftpClient = new SftpClient(new PasswordConnectionInfo(m_IpAddress, m_Username, m_Password));
+                m_SftpClient.Connect();
+            }
+            catch (Exception ex)
+            {
+                LogError("Failed to set up the SFTP connenction. Please check your server login information.");
+                LogError($"Detailed exception: {ex}");
+                Environment.Exit(1);
+            }
+
+            /** Finishing sign, that only displayed if everything went well. */
+            SshCommand readCommand = m_SshClient.RunCommand("uname -mrs");
+            LogMessage("Server's version: " + readCommand.Result);
         }
 
-        /** Deconstructor thats disconnects the API from server when the class gets deleted */
+        /** Deconstructor thats disconnects the Framework's SSH and SFTP clients from server
+         * when the class gets deleted from memory.*/
         ~ServerGatewayService()
         {
-            client.Disconnect();
+            m_SshClient.Disconnect();
+            m_SftpClient.Disconnect();
         }
 
-        /** Gets some basic informations about the servers operating system */
+        /** Gets some basic informations about the server's operating system. */
         public SshCommand GetServerInformations()
         {
-            return client.RunCommand("cat /etc/os-release");
+            return m_SshClient.RunCommand("cat /etc/os-release");
         }
 
-        /** Sends a command to the server and immediately executes it */
+        /** Sends a command to the server and immediately executes it.
+         * Most of the other functions in the Framework use this as a base function. */
         public SshCommand SendTask(string command)
         {
-            return client.RunCommand(command);
+            return m_SshClient.RunCommand(command);
         }
 
-        /** Creates a file on the server with the given arguements */
+        /** Creates a file on the server with the given path and
+         * writes in the given contents if there are. */
         public SshCommand CreateFile(string filePath, string fileContents = "")
         {
             return SendTask($"echo {fileContents} > {filePath}");
         }
 
-        /** Creates files on the server with the given arguements */
+        /** Creates files on the server with the given paths and
+         * writes in the given contents if there are. */
         public SshCommand[] CreateFiles(string[] filePaths, string[] fileContents = default)
         {
             SshCommand[] commands = default;
@@ -67,13 +101,13 @@ namespace ServerGateway
             return commands;
         }
 
-        /** Deletes a file on the server with the given arguement */
+        /** Deletes a file on the server with the given path. */
         public SshCommand DeleteFile(string filePath)
         {
             return SendTask($"rm {filePath}");
         }
 
-        /** Deletes files on the server with the given arguements */
+        /** Deletes files on the server with the given paths. */
         public SshCommand DeleteFiles(string[] filePaths)
         {
             StringBuilder pathBuilder = new StringBuilder();
@@ -87,16 +121,16 @@ namespace ServerGateway
         }
 
         /** Writes into the given file regardless if its exists.
-         * If the given file doesnt exists then the program does it first and
-         * write the arguements in afterwards */
+         * If the given file doesn't exists then the program does it first and
+         * write the contents in afterwards. */
         public SshCommand WriteIntoFile(string filePath, string fileContents)
         {
             return SendTask($"echo {fileContents} > {filePath}");
         }
 
         /** Writes into the given filse regardless if they exist.
-         * If the given files arent exist then the program does it first and
-         * write the arguements in afterwards */
+         * If the given files aren't exist then the program does it first and
+         * write the contents in afterwards. */
         public SshCommand[] WriteIntoFiles(string[] filePaths, string[] fileContents)
         {
             SshCommand[] commands = default;
@@ -107,13 +141,13 @@ namespace ServerGateway
             return commands;
         }
 
-        /** Appends the given contents to the given file */
+        /** Appends the given contents in the given file. */
         public SshCommand AppendTextToFile(string filePath, string fileContents)
         {
             return SendTask($"echo {fileContents} >> {filePath}");
         }
 
-        /** Appends the given contents to the given files */
+        /** Appends the given contents in the given files. */
         public SshCommand[] AppendTextToFiles(string[] filePaths, string[] fileContents)
         {
             SshCommand[] commands = default;
@@ -124,44 +158,88 @@ namespace ServerGateway
             return commands;
         }
 
-        /** Uploads a file to the server from the given path.
-         * The given path must be in your local machine (what from you running this API)
-         * The 'destinationPath' must have be located on the server */
+        /** Uploads a file to the server from the given local path.
+         * Then the uploaded file placed in the given destination path.
+         * The given path must be on your local machine (what from you running this Framework). */
         public void UploadFile(string localFilePath, string destinationPath)
         {
-            SftpClient client = new SftpClient(new PasswordConnectionInfo(m_IpAddress, m_Username, m_Password));
-            client.Connect();
-
             using (Stream stream = File.OpenRead(localFilePath))
             {
-                client.UploadFile(stream, destinationPath + Path.GetFileName(localFilePath), x =>
-                { Console.WriteLine($"Uploaded file name: [{Path.GetFileName(localFilePath)}] Uploaded file size (in bytes): [{x}]"); });
+                m_SftpClient.UploadFile(stream, destinationPath + Path.GetFileName(localFilePath), x =>
+                { LogMessage($"Uploaded file name: [{Path.GetFileName(localFilePath)}] Uploaded file size (in bytes): [{x}]"); });
             }
-
-            client.Disconnect();
         }
 
-        /** Downloads a file from the server to a destination path.
-         * The 'destinationPath' must be in your local machine (what from you running this API)
-         * The 'downloadableFilePath' must have be located on the server */
+        /** Uploads files to the server from the given local paths.
+         * Then the uploaded files placed in the given destination paths.
+         * The given paths must be on your local machine (what from you running this Framework). */
+        public void UploadFiles(string[] localFilePaths, string[] destinationPaths)
+        {
+            for (int i = 0; i < localFilePaths.Length; i++)
+            {
+                using (Stream stream = File.OpenRead(localFilePaths[i]))
+                {
+                    m_SftpClient.UploadFile(stream, destinationPaths[i] + Path.GetFileName(localFilePaths[i]), x =>
+                    { LogMessage($"Uploaded file name: [{Path.GetFileName(localFilePaths[i])}] Uploaded file size (in bytes): [{x}]"); });
+                }
+            }
+        }
+
+        /** Downloads a file from the server to the given local path.
+         * Then the downloaded file placed in the given local destination path.
+         * The given downloadable path must be on your server. */
         public void DownloadFile(string downloadableFilePath, string destinationPath)
         {
-            SftpClient client = new SftpClient(new PasswordConnectionInfo(m_IpAddress, m_Username, m_Password));
-            client.Connect();
-
             using (Stream stream = File.OpenWrite(destinationPath + Path.GetFileName(downloadableFilePath)))
             {
-                client.DownloadFile(downloadableFilePath, stream, x =>
-                { Console.WriteLine($"Downloaded file name: [{Path.GetFileName(downloadableFilePath)}] Downloaded file size (in bytes): [{x}]"); });
+                m_SftpClient.DownloadFile(downloadableFilePath, stream, x =>
+                { LogMessage($"Downloaded file name: [{Path.GetFileName(downloadableFilePath)}] Downloaded file size (in bytes): [{x}]"); });
             }
-
-            client.Disconnect();
         }
 
-        /** Private internal variables */
-        private readonly string m_Username;
-        private readonly string m_IpAddress;
-        private readonly string m_Password;
-        private readonly SshClient client;
+        /** Downloads files from the server to the given local paths.
+         * Then the downloaded files placed in the given local destination paths.
+         * The given downloadable paths must be on your server. */
+        public void DownloadFiles(string[] downloadableFilePaths, string[] destinationPaths)
+        {
+            for (int i = 0; i < downloadableFilePaths.Length; i++)
+            {
+                using (Stream stream = File.OpenWrite(destinationPaths[i] + Path.GetFileName(downloadableFilePaths[i])))
+                {
+                    m_SftpClient.DownloadFile(downloadableFilePaths[i], stream, x =>
+                    { LogMessage($"Downloaded file name: [{Path.GetFileName(downloadableFilePaths[i])}] Downloaded file size (in bytes): [{x}]"); });
+                }
+            }
+        }
+
+        /** Private internal functions that the class uses to print to the Console, if logging is enabled. */
+        internal void LogMessage(string message)
+        {
+            if (loggingEnabled_Message)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(message);
+            }
+        }
+        internal void LogError(string message)
+        {
+            if (loggingEnabled_Error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+            }
+        }
+
+        /** Public variables that determines, whether the Framework can log or not.
+         * They can be modified outside of the class. */
+        public bool loggingEnabled_Message = true;
+        public bool loggingEnabled_Error = true;
+
+        /** Private internal variables to get hold of the reusable information */
+        internal readonly string m_Username;
+        internal readonly string m_IpAddress;
+        internal readonly string m_Password;
+        internal readonly SshClient m_SshClient;
+        internal readonly SftpClient m_SftpClient;
     }
 }
